f3c1c42e9f50e73762bbbb8afaaa45ed73e26913
unknown <hazimbitw@gmail.com>
2022-03-11 16:22:59 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302204092/tpmodul3_1302204092/KodePos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204092
+{
+    public class KodePos
+    {
+        public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, 
+            Margasari, Sekejati, Kebonwaru, Maleer, Samoja };
+
+        public int getKodePos(Kelurahan inputKelurahan)
+        {
+            int[] arrayKodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+
+            return arrayKodePos[ (int)inputKelurahan ];
+        }
+
+    }
+}
--- tpmodul3_1302204092/tpmodul3_1302204092/Program.cs
+++ tpmodul3_1302204092/tpmodul3_1302204092/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace tpmodul3_1302204092
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            KodePos kp1 = new KodePos();
+
+            KodePos.Kelurahan inputKelurahan = KodePos.Kelurahan.Kujangsari;
+
+            Console.WriteLine(kp1.getKodePos(inputKelurahan));
         }
     }
 }
